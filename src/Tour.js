3034dbef4729b3d502ff6e8c69242d1ed731f15f
Kevin Lyons <zinckev@gmail.com>
2021-07-12 12:33:28 -0700

    finished function of not interest button. added substring and read more button for info. Refresher is completed

--- src/Tour.js
+++ src/Tour.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
-const Tour = ({ id, name, image, info, price }) => {
+const Tour = ({ id, name, image, info, price, removeTour }) => {
+  const [readMore, setReadMore] = useState(false);
   return (
     <article className="single-tour">
       <img src={image} alt={name}></img>
@@ -9,8 +10,15 @@ const Tour = ({ id, name, image, info, price }) => {
           <h4>{name}</h4>
           <h4 className="tour-price">${price}</h4>
         </div>
-        <p>{info}</p>
-        <button className="delete-btn"> Not Interested </button>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}`}
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? "Read less" : "Read More"}
+          </button>
+        </p>
+        <button className="delete-btn" onClick={() => removeTour(id)}>
+          Not Interested
+        </button>
       </footer>
     </article>
   );
